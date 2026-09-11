bafdec56fc1a89330abf159f54fb379d985af976
Edmund Chang <edmundchang7@gmail.com>
2026-09-11 17:55:48 +0000

    initial Express server with two routes

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, web!");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("I am taking the COMPSCI 3260 course.");
+});
+
+app.get("/hello/:name", (req, res) => {
+  const { name } = req.params;
+  res.send(`Hello, ${name}!`);
+});
+
+app.get("/repeat/:word", (req, res) => {
+  const { word } = req.params;
+  res.send(`${word} ${word} ${word}`);
+});
+
+app.get("/count", (req, res) => {
+  const from = req.query.from ?? "1";
+  const to = req.query.to ?? "10";
+  res.send(`Counting from ${from} to ${to}.`);
+});
+
+app.get("/api/info", (req, res) => {
+  res.json({
+    course: "COMPSCI 326",
+    unit: 2,
+  });
+});
+
+app.get("/api/error", (req, res) => {
+  res.status(400).send("Bad request");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
